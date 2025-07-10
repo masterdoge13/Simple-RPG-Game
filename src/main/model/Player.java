4e2ce4da85298018ddf4c6adf2f473363d2fbd41
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-10 16:01:07 -0700

    Added Player class and specification

---
+++ src/main/model/Player.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Player {
+
+    //EFFECTS: creates a player with the given name
+    public Player(String name) {
+
+    }
+    
+    //EFFECTS: returns the player's health
+    public int getHealth() {
+        return -1; //stub
+    }
+
+    //EFFECTS: returns the player's attack
+    public int getAttack() {
+        return -1; //stub
+    }
+
+    //EFFECTS: returns the player's name
+    public String getName() {
+        return "";
+    }
+}
