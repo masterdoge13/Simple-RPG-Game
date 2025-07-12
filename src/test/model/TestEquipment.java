71f73a21f61ad0dfb3fb0766288fe095144e086c
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-12 02:00:42 -0700

    Added Equipment tests for new methods

--- src/test/model/TestEquipment.java
+++ src/test/model/TestEquipment.java
@@ -19,17 +19,109 @@ public class TestEquipment {
     @Test
     void testConstructorSword() {
         assertEquals("test sword", testSword.getName());
-        assertEquals(1.5, testSword.getAttackMod(), 0.01);
-        assertEquals(0.0, testSword.getDefenseMod(), 0.01);
+        assertEquals(1.5, testSword.getAttackMod(), 0.001);
+        assertEquals(0.0, testSword.getDefenseMod(), 0.001);
         assertEquals(EquipmentType.SWORD, testSword.getType());
+        assertEquals(0, testSword.getUpgradeNum());
     }
 
     @Test
     void testConstructorArmour() {
         assertEquals("test armour", testArmour.getName());
-        assertEquals(0.0, testArmour.getAttackMod(), 0.01);
-        assertEquals(0.2, testArmour.getDefenseMod(), 0.01);
+        assertEquals(0.0, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.2, testArmour.getDefenseMod(), 0.001);
         assertEquals(EquipmentType.ARMOUR, testArmour.getType());
+        assertEquals(0, testArmour.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeSword() {
+        testSword.incrementUpgrade();
+        assertEquals(1.6, testSword.getAttackMod(), 0.001);
+        assertEquals(0.01, testSword.getDefenseMod(), 0.001);
+        assertEquals(1, testSword.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeSwordMultiple() {
+        testSword.incrementUpgrade();
+        assertEquals(1.6, testSword.getAttackMod(), 0.001);
+        assertEquals(0.01, testSword.getDefenseMod(), 0.001);
+        assertEquals(1, testSword.getUpgradeNum());
+        testSword.incrementUpgrade();
+        assertEquals(1.7, testSword.getAttackMod(), 0.001);
+        assertEquals(0.02, testSword.getDefenseMod(), 0.001);
+        assertEquals(2, testSword.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeSwordMax() {
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        assertEquals(2.0, testSword.getAttackMod(), 0.001);
+        assertEquals(0.05, testSword.getDefenseMod(), 0.001);
+        assertEquals(5, testSword.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeSwordOverMax() {
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        testSword.incrementUpgrade();
+        assertEquals(2.0, testSword.getAttackMod(), 0.001);
+        assertEquals(0.05, testSword.getDefenseMod(), 0.001);
+        assertEquals(5, testSword.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeArmour() {
+        testArmour.incrementUpgrade();
+        assertEquals(0.1, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.21, testArmour.getDefenseMod(), 0.001);
+        assertEquals(1, testArmour.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeArmourMultiple() {
+        testArmour.incrementUpgrade();
+        assertEquals(0.1, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.21, testArmour.getDefenseMod(), 0.001);
+        assertEquals(1, testArmour.getUpgradeNum());
+        testArmour.incrementUpgrade();
+        assertEquals(0.2, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.22, testArmour.getDefenseMod(), 0.001);
+        assertEquals(2, testArmour.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeArmourMax() {
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        assertEquals(0.5, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.25, testArmour.getDefenseMod(), 0.001);
+        assertEquals(5, testArmour.getUpgradeNum());
+    }
+
+    @Test
+    void testIncrementUpgradeArmourOverMax() {
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        testArmour.incrementUpgrade();
+        assertEquals(0.5, testArmour.getAttackMod(), 0.001);
+        assertEquals(0.25, testArmour.getDefenseMod(), 0.001);
+        assertEquals(5, testArmour.getUpgradeNum());
     }
 
     @Test
@@ -44,22 +136,22 @@ public class TestEquipment {
 
     @Test
     void testGetAttackModSword() {
-        assertEquals(1.5, testSword.getAttackMod(), 0.01);
+        assertEquals(1.5, testSword.getAttackMod(), 0.001);
     }
 
     @Test
     void testGetAttackModArmour() {
-        assertEquals(0.0, testArmour.getAttackMod(), 0.01);
+        assertEquals(0.0, testArmour.getAttackMod(), 0.001);
     }
 
     @Test
     void testGetDefenseModSword() {
-        assertEquals(0.0, testSword.getDefenseMod(), 0.01);
+        assertEquals(0.0, testSword.getDefenseMod(), 0.001);
     }
 
     @Test
     void testGetDefenseModArmour() {
-        assertEquals(0.2, testArmour.getDefenseMod(), 0.01);
+        assertEquals(0.2, testArmour.getDefenseMod(), 0.001);
     }
 
     @Test
@@ -71,4 +163,15 @@ public class TestEquipment {
     void testGetEquipmentTypeArmour() {
         assertEquals(EquipmentType.ARMOUR, testArmour.getType());
     }
+
+    @Test
+    void testGetUpgradeNumSword() {
+        assertEquals(0, testSword.getUpgradeNum());
+    }
+
+    @Test
+    void testGetUpgradeNumArmour() {
+        assertEquals(0, testArmour.getUpgradeNum());
+    }
+
 }
