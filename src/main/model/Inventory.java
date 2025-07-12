6fb93108f3ceffdcecaa77746d6efe44160a4568
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-12 03:49:53 -0700

    Added Inventory class and specification

---
+++ src/main/model/Inventory.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Inventory {
+
+    // EFFECTS: creates an empty inventory
+    public Inventory() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds an equipment to the inventory
+    public void insertEquipment() {
+        //stub
+    }
+
+    // REQUIRES: the inventory has an equipment at the index invIndex
+    // MODIFIES: this
+    // EFFECTS: removes an equipment at the specified index
+    public void removeEquipment(int invIndex) {
+        //stub
+    }
+
+    // EFFECTS: returns a string of all equipment in inventory
+    public String inventoryToString() {
+        return "";//stub
+    }
+
+    // REQUIRES: the inventory has an equipment at the index invIndex
+    // EFFECTS: returns the equipment at the specified index
+    public Equipment getEquipment(int invIndex) {
+        return new Equipment(null, 0.0, 0.0, EquipmentType.SWORD);//stub
+    }
+}
