591e58c6ef8caef1558101dba371285cbf5a51d3
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-12 04:42:01 -0700

    Added Inventory implementation

--- src/main/model/Inventory.java
+++ src/main/model/Inventory.java
@@ -1,38 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Inventory {
 
+    private ArrayList<Equipment> inventoryArray;
     // EFFECTS: creates an empty inventory
     public Inventory() {
-        //stub
+        inventoryArray = new ArrayList<Equipment>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds an equipment to the inventory
     public void insertEquipment(Equipment item) {
-        //stub
+        inventoryArray.add(item);
     }
 
     // REQUIRES: the inventory has an equipment at the index invIndex
     // MODIFIES: this
     // EFFECTS: removes an equipment at the specified index
     public void removeEquipment(int invIndex) {
-        //stub
+        inventoryArray.remove(invIndex);
     }
 
     // EFFECTS: returns a string of all equipment in inventory
     public String inventoryToString() {
-        return "";//stub
+        String items = "";
+        int count = 0;
+        for (Equipment equipment : inventoryArray) {
+            items = items + count + ") " + equipment.equipmentToString() + "\n";
+            count++;
+        }
+        items = "Inventory:\n" + items;
+        return items;
     }
 
     // REQUIRES: the inventory has an equipment at the index invIndex
     // EFFECTS: returns the equipment at the specified index
     public Equipment getEquipment(int invIndex) {
-        return new Equipment(null, 0.0, 0.0, EquipmentType.SWORD);//stub
+        return inventoryArray.get(invIndex);
     }
 
     // EFFECTS: returns the size of the inventory
     public int getSize() {
-        return -1;//stub
+        return inventoryArray.size();
     }
 }
