55f41efacf4b9f9f353e129903830e99de512efe
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-16 22:32:50 -0700

    Added Difficulty class and specification

---
+++ src/main/model/Difficulty.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Difficulty {
+
+    // EFFECTS: creates a difficulty modifier
+    public Difficulty() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increments the difficulty number
+    public void increaseDifficulty() {
+        //stub
+    }
+
+    // EFFECTS: returns the equipment attack modifier difficulty increase
+    public double getEquipmentAttackMod() {
+        return 0.0;//stub
+    }
+
+    // EFFECTS: returns the equipment attack modifier difficulty increase
+    public double getEquipmentDefenseMod() {
+        return 0.0;//stub
+    }
+
+    // EFFECTS: returns the difficulty number
+    public int getDifficulty() {
+        return -1;//stub
+    }
+}
