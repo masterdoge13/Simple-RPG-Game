87ada7bbcf0f9b2987025daea6b9c7e17b4d8ca2
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-18 20:56:45 -0700

    Added Combat class and specification

---
+++ src/main/ui/Combat.java
@@ -0,0 +1,60 @@
+package ui;
+
+import java.util.Scanner;
+import model.Enemy;
+import model.Player;
+
+public class Combat {
+
+    // MODIFIES: this
+    // EFFECTS: Initializes combat with player data
+    public Combat(Player player) {
+        //stub TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runCombat() {
+        //stub TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes enemy data
+    private void init() {
+        //stub TODO
+    }
+
+    // EFFECTS: processes user command in main menu
+    private void processCommandMain(String command) {
+        //stub TODO
+    }
+
+    // EFFECTS: displays main menu options
+    private void displayMainMenu() {
+        //stub TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: gives rewards for winning, increases difficulty and exits combat
+    private void winCombat() {
+        //stub TODO
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: decreases difficulty for losing and exit combat
+    private void loseCombat() {
+        //stub TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: attack the enemy
+    private void attack() {
+        //stub TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: enemy attacks the player
+    private void getAttacked() {
+        //stub TODO
+    }
+}
