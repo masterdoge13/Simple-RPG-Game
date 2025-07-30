131a970a37dfb5d9798a2c27975558a9ba841d63
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-29 21:55:22 -0700

    Added PlayerNameGUI class and specifications

---
+++ src/main/ui/PlayerNameGUI.java
@@ -0,0 +1,26 @@
+package ui;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+
+
+// pop-up at the beginning of the game to get player name
+// template taken from AlarmControllerUI class in AlarmSystem
+public class PlayerNameGUI extends JFrame {
+
+    // EFFECTS: creates the pop-up window
+    public PlayerNameGUI() {
+        //stub
+    }
+
+    // the pop-up window
+    private class NameInput extends AbstractAction {
+
+        @Override
+		public void actionPerformed(ActionEvent evt) {
+            //stub
+        }
+    }
+}
