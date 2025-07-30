6f464694e9390d32ac3671ffa160831bccf4662c
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-30 00:11:13 -0700

    Added specification for MainMenuGUI

---
+++ src/main/ui/MainMenuGUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.JFrame;
+
+import model.Player;
+
+// GUI for the main menu of the game
+public class MainMenuGUI extends JFrame {
+
+    // EFFECTS: creates a main menu GUI with a player
+    public MainMenuGUI(Player player) {
+        //stub
+    }
+
+    // template taken from AlarmControllerUI in AlarmSystem
+    // EFFECTS: creates a panel of buttons for
+    private void addButtonPanel() {
+        //stub
+    }
+}
