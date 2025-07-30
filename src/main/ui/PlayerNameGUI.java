8c6a728dda2ba013aa2d518e252a890b69e6cf57
Edwin Lui <elui01@students.cs.ubc.ca>
2025-07-29 23:05:56 -0700

    Fixed Checkstyle errors

--- src/main/ui/PlayerNameGUI.java
+++ src/main/ui/PlayerNameGUI.java
@@ -4,13 +4,11 @@ import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 import model.Player;
 
 import java.awt.event.ActionEvent;
-
 
 // pop-up at the beginning of the game to get player name
 // template taken from AlarmControllerUI class in AlarmSystem
@@ -20,7 +18,8 @@ public class PlayerNameGUI extends JFrame {
     private static final int BASE_HEALTH = 100;
     private static final int STARTING_STAT_POINTS = 5;
     private static final int WIDTH = 800;
-	private static final int HEIGHT = 600;
+    private static final int HEIGHT = 600;
+
     private Player player;
 
     // EFFECTS: creates the pop-up window
@@ -28,10 +27,8 @@ public class PlayerNameGUI extends JFrame {
         add(new JButton(new NameInput()));
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setVisible(true);
+        setVisible(true);
     }
-
-
 
     // the pop-up window and player initialization
     private class NameInput extends AbstractAction {
@@ -41,15 +38,15 @@ public class PlayerNameGUI extends JFrame {
         }
 
         @Override
-		public void actionPerformed(ActionEvent evt) {
-			String playerName = JOptionPane.showInputDialog(null,
-					  "What is your name?",
-					  "Hero Maker",
-					  JOptionPane.QUESTION_MESSAGE);
+        public void actionPerformed(ActionEvent evt) {
+            String playerName = JOptionPane.showInputDialog(null,
+                    "What is your name?",
+                    "Hero Maker",
+                    JOptionPane.QUESTION_MESSAGE);
             player = new Player(playerName, BASE_ATTACK, BASE_HEALTH);
             player.increaseStatPoints(STARTING_STAT_POINTS);
             dispose();
-            System.out.println(player.getEquipped().equippedToString()); //placeholder test print statement
+            System.out.println(player.getEquipped().equippedToString()); // placeholder test print statement
 
         }
     }
