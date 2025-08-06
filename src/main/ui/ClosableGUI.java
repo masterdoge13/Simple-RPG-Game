c56c438f5b54471133bafd38a25d7719da832d64
Edwin Lui <elui01@students.cs.ubc.ca>
2025-08-05 19:14:36 -0700

    Added stubs for unimplemented methods in interface

--- src/main/ui/ClosableGUI.java
+++ src/main/ui/ClosableGUI.java
@@ -1,13 +1,53 @@
 package ui;
 
-import javax.swing.JFrame;
+
+import model.EventLog;
+import model.Event;
+
 import java.awt.event.*;
 
-// an abstract class that allows for actions when the window is closed
-public abstract class ClosableGUI implements WindowListener {
+import javax.swing.JFrame;
 
+// an abstract class that allows for actions when the window is closed
+public abstract class ClosableGUI extends JFrame implements WindowListener {
+
+    // taken from ScreenPrinter in AlarmSystem
     // EFFECTS: prints out event log on window close
     public void windowClosing(WindowEvent e) {
-        //stub
+
+        for (Event next : EventLog.getInstance()) {
+            System.out.println(next.toString());
+        }
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        // stub
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        // stub
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        // stub
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        // stub
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        // stub
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        // stub
     }
 }
